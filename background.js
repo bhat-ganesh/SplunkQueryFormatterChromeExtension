006394da941829b8df489f80b5ffabd9ba464ab0
Ganesh Bhat <ganesbha@cisco.com>
2017-01-10 11:40:36 -0500

    cleaned up added splunk webpage

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-    if (tab.url.indexOf("https://www.stb.bskyb.com/splunk/") != -1) { // Inspect whether the place where user clicked matches with our list of URL
-        chrome.tabs.executeScript(tab.id, {
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.executeScript(
+        tab.id, 
+        {
             "file": "sqf.js"
-        }, function () { // Execute your code
-            console.log("Script Executed .. "); // Notification on Completion
-        });
-    }
+        }, 
+        function () {}
+    );
 });
--- sqf.js
+++ sqf.js
@@ -1,3 +1,1 @@
-//alert("Code Executed ... ");
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Search", "g"), "--- CHANGED ---");
-document.getElementsByName("q")[0].innerHTML = document.getElementsByName("q")[0].innerHTML.replace(new RegExp("\\|", "g"), "\n\|");
+document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\|", "g"), "\n\|");
