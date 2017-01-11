47b1593246152a59fe02e677606ab16b42f44dc8
Ganesh Bhat <ganesbha@cisco.com>
2017-01-11 15:00:48 -0500

    added whitespace management extra new line for join

--- js/sqf.js
+++ js/sqf.js
@@ -1,1 +1,3 @@
 document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\|", "g"), "\n\|");
+document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp(" +", "g"), " ");
+document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\| join ", "g"), "\n\| join ");
