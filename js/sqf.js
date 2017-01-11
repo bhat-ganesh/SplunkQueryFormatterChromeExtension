c55f025de59a925cecc0f1e272bc951b22f73ab5
Ganesh Bhat <ganesbha@cisco.com>
2017-01-11 15:54:58 -0500

    fixed new line after ]

--- js/sqf.js
+++ js/sqf.js
@@ -1,4 +1,4 @@
 document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\|", "g"), "\n\|");
 document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp(" +", "g"), " ");
 document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\| join ", "g"), "\n\| join ");
-document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\] $", "g"), " \n");
+document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\] ", "g"), "\] \n");
