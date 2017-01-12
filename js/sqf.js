1419359a79a93fe30a20330b47f6358b33b58ada
Ganesh Bhat <ganesbha@cisco.com>
2017-01-12 14:43:45 -0500

    added formatting to Edit Search window

--- js/sqf.js
+++ js/sqf.js
@@ -1,4 +1,13 @@
-document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\|", "g"), "\n\|");
-document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp(" +", "g"), " ");
-document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\| join ", "g"), "\n\| join ");
-document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\] ", "g"), "\] \n");
+if(document.getElementsByName("q")[0]) {
+    document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\|", "g"), "\n\|");
+    document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp(" +", "g"), " ");
+    document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\| join ", "g"), "\n\| join ");
+    document.getElementsByName("q")[0].value = document.getElementsByName("q")[0].value.replace(new RegExp("\\] ", "g"), "\] \n");
+}
+
+if(document.getElementsByName("search")[0]) {
+    document.getElementsByName("search")[0].value = document.getElementsByName("search")[0].value.replace(new RegExp("\\|", "g"), "\n\|");
+    document.getElementsByName("search")[0].value = document.getElementsByName("search")[0].value.replace(new RegExp(" +", "g"), " ");
+    document.getElementsByName("search")[0].value = document.getElementsByName("search")[0].value.replace(new RegExp("\\| join ", "g"), "\n\| join ");
+    document.getElementsByName("search")[0].value = document.getElementsByName("search")[0].value.replace(new RegExp("\\] ", "g"), "\] \n");
+}
